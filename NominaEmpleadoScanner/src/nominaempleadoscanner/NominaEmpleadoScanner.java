204cf4f69c54831fcfca6cdccc332adf16550f8a
David <luisdavid.vp@gmail.com>
2018-09-27 22:21:00 +0200

    Nómina Empleado usando Scanner, completo

--- NominaEmpleadoScanner/src/nominaempleadoscanner/NominaEmpleadoScanner.java
+++ NominaEmpleadoScanner/src/nominaempleadoscanner/NominaEmpleadoScanner.java
@@ -25,7 +25,24 @@ public class NominaEmpleadoScanner {
         byte horasExtraTrabajadas = sc.nextByte();
         System.out.printf("Precio hora: ");
         float precioHora = sc.nextFloat();
+        
+        //PROCESO
 
+        float horasExtra = horasExtraTrabajadas * precioHora;
+        float descuentos = porcentajeDescuento * (sueldo + horasExtra) / 100;
+        float diferencia = sueldo - descuentos;
+        float sueldoNeto = diferencia + horasExtra;
+
+        //SALIDA
+        
+        System.out.printf("Mes: %s %n", mes);
+        System.out.printf("Nómina del empleado: %s %n", empleado);
+        System.out.printf("Porcentaje de descuento: %.2f%% %n", porcentajeDescuento);
+        System.out.printf("Horas Extras: %.2f %n", horasExtra);
+        System.out.printf("Descuentos: %.2f %n", descuentos);
+        System.out.printf("Diferencia: %.2f %n", diferencia);
+        System.out.printf("Sueldo Neto: %.2f %n", sueldoNeto);
+        
     }
 
 }
