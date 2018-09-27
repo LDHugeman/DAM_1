4a9fe5d19b2f94b5c6e1d1ee5c64f5efa29bb2a5
David <luisdavid.vp@gmail.com>
2018-09-27 19:37:34 +0200

    Nómina Empleado usando BufferedReader, completo

---
+++ NominaEmpleadoScanner/src/nominaempleadoscanner/NominaEmpleadoScanner.java
@@ -0,0 +1,19 @@
+package nominaempleadoscanner;
+
+import java.util.*;
+
+/**
+ *
+ * @author David
+ */
+public class NominaEmpleadoScanner {
+
+    public static void main(String[] args) {
+        
+        //ENTRADA
+        
+        Scanner sc=new Scanner(System.in);
+
+    }
+
+}
