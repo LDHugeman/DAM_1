bc0009e3a72d63ea6366e53cf8122d44565a3eb0
David <luisdavid.vp@gmail.com>
2018-09-27 20:20:04 +0200

    Nómina Empleado usando Scanner

--- NominaEmpleadoScanner/src/nominaempleadoscanner/NominaEmpleadoScanner.java
+++ NominaEmpleadoScanner/src/nominaempleadoscanner/NominaEmpleadoScanner.java
@@ -9,10 +9,22 @@ import java.util.*;
 public class NominaEmpleadoScanner {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
         //ENTRADA
         
-        Scanner sc=new Scanner(System.in);
+        System.out.printf("Mes: ");
+        String mes = sc.nextLine();
+        System.out.printf("Nombre del empleado: ");
+        String empleado = sc.nextLine();
+        System.out.printf("Sueldo base: ");
+        float sueldo = sc.nextFloat(); 
+        System.out.println("Porcentaje de descuento: ");
+        float porcentajeDescuento = sc.nextFloat();
+        System.out.println("Horas extra trabajadas: ");
+        byte horasExtraTrabajadas = sc.nextByte();
+        System.out.printf("Precio hora: ");
+        float precioHora = sc.nextFloat();
 
     }
 
