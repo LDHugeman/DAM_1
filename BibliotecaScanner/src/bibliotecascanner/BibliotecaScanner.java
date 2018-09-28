8423abebe977826a8e2e70df275aef00a1e74d68
David <luisdavid.vp@gmail.com>
2018-09-28 20:32:31 +0200

    Ejercicio Biblioteca con Scanner, completo.

--- BibliotecaScanner/src/bibliotecascanner/BibliotecaScanner.java
+++ BibliotecaScanner/src/bibliotecascanner/BibliotecaScanner.java
@@ -1,5 +1,6 @@
-
 package bibliotecascanner;
+
+import java.util.*;
 
 /**
  *
@@ -8,7 +9,32 @@ package bibliotecascanner;
 public class BibliotecaScanner {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //ENTRADA
         
+        System.out.printf("Mes: ");
+        String mes = sc.nextLine();
+        System.out.printf("Título del libro: ");
+        String titulo = sc.nextLine();
+        System.out.printf("Cantidad de libros prestados: ");
+        int cantidadLibrosPrestados = sc.nextInt();
+        System.out.printf("Número de consultas del libro en el mes: ");
+        byte numeroConsultas = sc.nextByte();
+        System.out.printf("Préstamos totales en el mes: ");
+        int prestamosTotales = sc.nextInt();
+
+        //PROCESO
+        
+        int tasaDeConsulta = (int) (numeroConsultas * 100) / prestamosTotales;
+
+        //SALIDA
+        
+        System.out.printf("El libro %s se consultó %d veces.%n", titulo, numeroConsultas);
+        System.out.printf("La tasa de consulta en el mes de %s es %d%% %n", mes, tasaDeConsulta);       
+
+
+
     }
-    
+
 }
