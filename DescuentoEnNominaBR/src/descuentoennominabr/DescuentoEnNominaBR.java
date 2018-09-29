8282aa6847e00ba1571208ec3323aa69069c5a8a
David <luisdavid.vp@gmail.com>
2018-09-29 16:47:52 +0200

    Ejercicio DescuentoEnNomina con BufferedReader, completo.

---
+++ DescuentoEnNominaBR/src/descuentoennominabr/DescuentoEnNominaBR.java
@@ -0,0 +1,34 @@
+
+package descuentoennominabr;
+import java.io.*;
+/**
+ *
+ * @author Luis David
+ */
+public class DescuentoEnNominaBR {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader lee = new BufferedReader(new InputStreamReader(System.in));
+        
+        //ENTRADA
+        
+        System.out.printf("Nombre del empleado: ");
+        String empleado = lee.readLine();
+        System.out.printf("Horas que faltó: ");
+        byte horasFaltadas = Byte.parseByte(lee.readLine());
+        
+        //PROCESO
+        
+        float descuento = (horasFaltadas/3)*30;
+        int horasPendientesProximoMes = horasFaltadas%3;
+        
+        //SALIDA
+        
+        System.out.printf("El empleado %s faltó %d %n", empleado, horasFaltadas);
+        System.out.printf("Le corresponde un descuento de %.2f euros.%n", descuento);
+        System.out.printf("Le restan %d para el próximo mes. %n",horasPendientesProximoMes);
+        
+              
+    }
+    
+}
