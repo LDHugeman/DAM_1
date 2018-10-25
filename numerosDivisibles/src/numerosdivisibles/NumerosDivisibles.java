e55da078138aa546c435f9f3ee999918937ecf36
David <luisdavid.vp@gmail.com>
2018-10-25 20:05:50 +0200

    Ejercicio terminado.

---
+++ numerosDivisibles/src/numerosdivisibles/NumerosDivisibles.java
@@ -0,0 +1,48 @@
+package numerosdivisibles;
+
+/**
+ *
+ * @author David
+ */
+public class NumerosDivisibles {
+    
+    public static void main(String[] args) {
+        divisibleFor(7);
+        divisibleWhile(7);
+        divisibleDoWhile(7);
+    }
+    
+    public static void divisibleFor(int divisor) {
+        for (int divisible = divisor; divisible <= 200; divisible += divisor) {
+            mostrarDivisible(divisible, divisor);
+        }
+        System.out.println("");
+    }
+    
+    public static void divisibleWhile(int divisor) {
+        int divisible = divisor;
+        while (divisible <= 200) {
+            mostrarDivisible(divisible, divisor);
+            divisible += divisor;
+        }
+        System.out.println("");
+    }
+    
+    public static void divisibleDoWhile(int divisor) {
+        int divisible = divisor;
+        do {
+            mostrarDivisible(divisible, divisor);
+            divisible += divisor;
+        } while (divisible <= 200);
+        System.out.println("");
+    }
+    
+    public static void mostrarDivisible(int divisible, int divisor) {
+        if (divisible == divisor) {
+            System.out.printf("%d", divisible);
+        } else {
+            System.out.printf(" - %d", divisible);
+        }
+    }
+    
+}
