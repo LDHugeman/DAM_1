1ba632ce1e7b61d11f491f004fd929e98197cea2
David <luisdavid.vp@gmail.com>
2019-01-20 20:44:48 +0100

    código limpo

--- Java 2ª Evaluación/CuentasCorriente/src/obxetos/Cuenta.java
+++ Java 2ª Evaluación/CuentasCorriente/src/obxetos/Cuenta.java
@@ -1,51 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package obxetos;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
  * @author Luis David
  */
 public class Cuenta {
+
     private String numero;
     private String sucursal;
     private ArrayList<Cliente> clientes;
-    
-    public Cuenta(String numero, String sucursal, Cliente primerCliente) throws IOException{
+
+    public Cuenta(String numero, String sucursal, Cliente primerCliente) throws IOException {
         this.numero = numero;
         this.sucursal = sucursal;
         this.clientes = new ArrayList<>();
         clientes.add(primerCliente);
     }
-    
-    public String getNumero(){
+
+    public String getNumero() {
         return numero;
     }
-    
-    public ArrayList<Cliente> getClientes(){
+
+    public ArrayList<Cliente> getClientes() {
         return clientes;
     }
 
     public String getSucursal() {
         return sucursal;
     }
-    
-    public void eliminarCliente(String dni){
-        for(Cliente cliente : clientes){
-            if(cliente.getDNI().equals(dni)){
-                System.out.println("Eliminado cliente "+cliente.getDNI()+" de la cuenta "+this.getNumero());
-                clientes.remove(cliente);                
+
+    public void eliminarCliente(String dni) {
+        Cliente clienteABorrar= null;
+        Iterator it = clientes.iterator();
+        while (it.hasNext()) {
+            Cliente cliente = (Cliente) it.next();
+            if (cliente.getDNI().equals(dni)) {
+                clienteABorrar = cliente;
+                System.out.println("Eliminado cliente " + cliente.getDNI() + " de la cuenta " + this.getNumero());               
             }
         }
-    }
-
-    
-    
-    
+        clientes.remove(clienteABorrar);
+    }   
 }
