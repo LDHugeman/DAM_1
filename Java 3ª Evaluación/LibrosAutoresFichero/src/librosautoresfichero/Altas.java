b1074970939e68f04e88843f3a8bf6d156b52bf6
David <luisdavid.vp@gmail.com>
2019-03-31 23:45:16 +0200

    Ejericio con Altas y Bajas

--- Java 3ª Evaluación/LibrosAutoresFichero/src/librosautoresfichero/Altas.java
+++ Java 3ª Evaluación/LibrosAutoresFichero/src/librosautoresfichero/Altas.java
@@ -5,7 +5,6 @@ import Objetos.ObjectOutputStreamSinCabecera;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 
 /**
  *
--- Java 3ª Evaluación/LibrosAutoresFichero/src/librosautoresfichero/Menu.java
+++ Java 3ª Evaluación/LibrosAutoresFichero/src/librosautoresfichero/Menu.java
@@ -4,7 +4,6 @@ import Objetos.Autor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
  *
